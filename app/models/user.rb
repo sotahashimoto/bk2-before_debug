a4dc753527d61ae2287283f3fc031afa2f1c5a74
vagrant <sotahashimoto316@gmail.com>
2020-08-02 00:59:37 +0000

    favoritesAdd

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :favorites
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end
 end
