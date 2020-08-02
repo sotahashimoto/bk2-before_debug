3010438ad4520a114298312405c2bffddd125442
vagrant <sotahashimoto316@gmail.com>
2020-08-02 06:34:55 +0000

    task4finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_010656) do
+ActiveRecord::Schema.define(version: 2020_08_02_052128) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_08_02_010656) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
